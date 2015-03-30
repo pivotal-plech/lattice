6457acc2a0f4ee05cd8395006d546ff9703e4a3e
David Wadden <dwadden@pivotal.io>
2015-03-29 23:17:13 -0500

    wip - tee2metron changes

--- tee2metron/test_helpers/chatty_process/main.go
+++ tee2metron/test_helpers/chatty_process/main.go
@@ -4,12 +4,36 @@ import (
 	"fmt"
 	"os"
 	"time"
+	"io"
 )
 
 func main() {
+	done := make(chan struct{})
+
+	go pollStdin(os.Stdin, done)
+
+	loop:
 	for {
+		fmt.Println("loop")
 		fmt.Fprintf(os.Stdout, "Hi from stdout. My args are: %s\n", os.Args[1:])
 		fmt.Fprintln(os.Stderr, "Oopsie from stderr")
 		time.Sleep(100 * time.Millisecond)
+
+		select {
+		case <- done:
+			close(done)
+			break loop
+		default:
+		}
 	}
+
+	panic("wtf")
+
+}
+
+func pollStdin(stdinReader io.ReadCloser, done chan struct{}) () {
+	defer stdinReader.Close()
+
+	fmt.Fscanln(os.Stdin)
+	done <- struct{}{}
 }
